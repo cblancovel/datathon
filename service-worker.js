61b19b1a57152d149c61ecf75dcbadaa490e2a9f
cblancovel <CBLANCOVEL@GMAIL.COM>
2025-09-10 12:47:08 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+// Network-only service worker (evita problemas de caché en GitHub Pages)
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
